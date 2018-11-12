315ba962016c71ac4dc0ad0e11a0320d034d2443
Jon Magbadelo <jm676@sussex.ac.uk>
2018-11-12 12:36:52 +0000

    First commit

---
+++ src/main/java/com/magbadelo/jonathan/i2cscw/CipherTextDecrypter.java
@@ -0,0 +1,18 @@
+package com.magbadelo.jonathan.i2cscw;
+
+public class CipherTextDecrypter {
+    public static void main(String[] args) {
+        String cipherText = "PBFPVYFBQXZTYFPBFEQJHDXXQVAPTPQJKTOY\n" +
+                "QWIPBVWLXTOXBTFXQWAXBVCXQWAXFQJV\n" +
+                "WLEQNTOZQGGQLFXQWAKVWLXQWAEBIPBFX\n" +
+                "FQVXGTVJVWLBTPQWAEBFPBFHCVLXBQUFEVW\n" +
+                "LXGDPEQVPQGVPPBFTIXPFHXZHVFAGFOTHFEFB\n" +
+                "QUFTDHZBQPOTHXTYFTODXQHFTDPTOGHFQP\n" +
+                "BQWAQJJTODXQHFOQPWTBDHHIXQVAPBFZQ\n" +
+                "HCFWPFHPBFIPBQWKFABVYYDZBOTHPBQPQJT\n" +
+                "QOTOGHFQAPBFEQJHDXXQVAVXEBQPEFZBVF\n" +
+                "OJIWFFACFCCFHQWAUVWFLQHGFXVAFXQHFU\n" +
+                "FHILTTAVWAFFAWTEVOITDHFHFQAITIXPFHX\n" +
+                "AFQHEFZQWGFLVWPTOFFA";
+    }
+}
